2f8b4f222cc8c7b5ec1fa53a759e675549c1fd98
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2022-11-23 10:37:04 -0600

    Renames project

--- Gerulus.Standalone/Program.cs
+++ Gerulus.Standalone/Program.cs
@@ -9,7 +9,7 @@ namespace SecureMessenger.Terminal;
 
 public class Program
 {
-    public const string ConnectionString = "Data Source=EncryptedMessenger.db";
+
 
     public static void Main()
     {
@@ -28,21 +28,7 @@ public class Program
         var username = Prompt.Input<string>("Username");
         var password = Prompt.Password("Password");
 
-        using (var conn = new SqliteConnection(ConnectionString))
-        {
-            conn.Open();
-            using (var command = new SqliteCommand($"SELECT PASSWORD FROM USERS WHERE USERNAME = '{username}';", conn))
-            {
-                Console.WriteLine(command.CommandText);
-                var correctPassword = command.ExecuteScalar();
-                if (correctPassword.Equals(password))
-                    Console.WriteLine("Authenticated");
-                else
-                    Console.WriteLine("Bad login");
-            }
 
-            conn.Close();
-        }
     }
 }
 
