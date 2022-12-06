10828649d52a7ad9852da7014561a48206052f03
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2022-12-06 08:11:41 -0600

    Switches DI provider to Autofac and splits Application class into separate file

---
+++ Gerulus.Standalone/Application.cs
@@ -0,0 +1,85 @@
+using Autofac;
+using Gerulus.Core.Auth;
+using Gerulus.Core.Crypto;
+using Gerulus.Standalone.UserForms;
+
+namespace Gerulus.Standalone;
+
+public class Application : IDisposable, IAsyncDisposable
+{
+    private IContainer? Container { get; set; }
+    private ILifetimeScope? ServicesScope { get; set; }
+
+    public Application()
+    {
+        var builder = new ContainerBuilder();
+        builder.RegisterType<DHFGKeyProvider>()
+               .As<ICryptoKeyProvider>()
+               .As<ICryptoKeyService<DHFGParameters>>();
+
+        builder.RegisterType<EncryptionMessageService>()
+               .As<IEncryptionMessageService>();
+
+        builder.RegisterType<AuthenticationService>()
+               .As<IAuthenticationService>();
+
+        builder.RegisterAssemblyTypes(typeof(IUserForm).Assembly)
+               .Where(type => typeof(IUserForm).IsAssignableFrom(type))
+               .AsSelf();
+
+        Container = builder.Build();
+        ServicesScope = Container.BeginLifetimeScope();
+    }
+
+    public Task RunAsync()
+    {
+        if (ServicesScope is null)
+            throw new InvalidOperationException("Application has already been disposed");
+
+        var form = ServicesScope.Resolve<MainMenuForm>();
+        return form.ExecuteAsync();
+    }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await DisposeAsyncCore().ConfigureAwait(false);
+
+        Dispose(false);
+#pragma warning disable CA1816 // Dispose methods should call SuppressFinalize
+        GC.SuppressFinalize(this);
+#pragma warning restore CA1816 // Dispose methods should call SuppressFinalize
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            ServicesScope?.Dispose();
+            Container?.Dispose();
+
+            ServicesScope = null;
+            Container = null;
+        }
+    }
+
+    protected virtual async ValueTask DisposeAsyncCore()
+    {
+        if (ServicesScope is not null)
+            await ServicesScope.DisposeAsync().ConfigureAwait(false);
+
+        if (Container is not null)
+            await Container.DisposeAsync().ConfigureAwait(false);
+
+        ServicesScope = null;
+        Container = null;
+    }
+
+}
+
--- Gerulus.Standalone/Program.cs
+++ Gerulus.Standalone/Program.cs
@@ -1,55 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Net;
-using System.Runtime.CompilerServices;
-using Gerulus.Core;
-using Gerulus.Core.Auth;
-using Gerulus.Core.Crypto;
-using Gerulus.Standalone.UserForms;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyModel;
-using Org.BouncyCastle.Asn1.Misc;
-using Sharprompt;
-
-namespace Gerulus.Standalone;
-
-public class Application
-{
-    private IServiceProvider Services { get; }
-    public Application()
-    {
-        var collection = new ServiceCollection();
-        collection.AddDbContext<GerulusContext>();
-
-        collection.AddSingleton<IEncryptionMessageService, EncryptionMessageService>();
-        collection.AddSingleton<ICryptoKeyService<DHFGParameters>, DHFGKeyProvider>();
-        collection.AddSingleton<IAuthenticationService, AuthenticationService>();
-
-        var forms = new List<Type>()
-        {
-            typeof(MainMenuForm),
-            typeof(CreateAccountUserForm), typeof(LoginUserForm),
-            typeof(InboxUserForm)
-        };
-
-        foreach (var form in forms)
-        {
-            collection.AddSingleton(form, form);
-        }
-
-        Services = collection.BuildServiceProvider();
-    }
-
-    public Task RunAsync()
-    {
-        return Services.GetRequiredService<MainMenuForm>().ExecuteAsync();
-    }
-}
+﻿namespace Gerulus.Standalone;
 
 public class Program
 {
     public static async Task Main()
     {
-        var app = new Application();
+        using var app = new Application();
         await app.RunAsync();
     }
 }
