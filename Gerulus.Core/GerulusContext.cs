5fc4ff54a4ecdb4ad80e9cdaedd18470845c1c9b
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2022-12-06 08:27:27 -0600

    Adds message entity to the database context

--- Gerulus.Core/GerulusContext.cs
+++ Gerulus.Core/GerulusContext.cs
@@ -5,6 +5,7 @@ namespace Gerulus.Core;
 public class GerulusContext : DbContext
 {
     public DbSet<User> Users => Set<User>();
+    public DbSet<Message> Messages => Set<Message>();
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
