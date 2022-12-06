2cf402b4bec7680b9fc54b6fb67beba772b4c48f
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2022-12-06 08:28:06 -0600

    Adds necessary annotations and constructor for EF

--- Gerulus.Core/Message.cs
+++ Gerulus.Core/Message.cs
@@ -1,14 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Gerulus.Core;
 
 public class Message
 {
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public long Id { get; init; }
+
     public required User Author { get; init; }
     public required User Recipient { get; init; }
     public required byte[] Contents { get; init; }
     public required byte[] IV { get; init; }
     public DateTimeOffset SentTime { get; }
     public bool IsRead { get; set; } = false;
+
+    private Message()
+    {
+    }
 
     public Message(DateTimeOffset? sentTime = null)
     {
