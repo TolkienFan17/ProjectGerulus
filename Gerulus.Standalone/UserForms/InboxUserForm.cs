d87ea27dde2452bf13f78aed0d53ac46992bc87c
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2022-12-12 15:17:07 -0600

    Creates a blank inbox form

--- Gerulus.Standalone/UserForms/InboxUserForm.cs
+++ Gerulus.Standalone/UserForms/InboxUserForm.cs
@@ -2,9 +2,16 @@ namespace Gerulus.Standalone.UserForms;
 
 public class InboxUserForm : IUserForm
 {
+    public required LocalUserState UserState { get; init; }
+
+    public InboxUserForm(LocalUserState userState)
+    {
+        UserState = userState;
+    }
+
     public Task ExecuteAsync()
     {
-        Console.WriteLine("Render inbox");
+        Console.WriteLine($"Welcome, {UserState.CurrentUser!.Username}! You have no messages in your inbox.");
         return Task.CompletedTask;
     }
 }
