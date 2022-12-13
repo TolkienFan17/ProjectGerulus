ff9c0bc22b57ccf9c99a4faf536fe8a3d508a834
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2022-12-13 16:58:11 -0600

    Switches contact id from a planned composite key to a generated id

--- Gerulus.Core/Contacts/Contact.cs
+++ Gerulus.Core/Contacts/Contact.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Gerulus.Core.Contacts;
 
 public class Contact
 {
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; init; }
+
     public required User PrincipalUser { get; init; }
     public required User IntendedUser { get; init; }
 
