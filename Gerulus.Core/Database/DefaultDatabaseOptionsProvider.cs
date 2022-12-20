434dec691fc11827aa0b68c0128127121790c7db
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2022-12-20 15:45:41 -0600

    Changes default database options provider to use configured database file location

--- Gerulus.Core/Database/DefaultDatabaseOptionsProvider.cs
+++ Gerulus.Core/Database/DefaultDatabaseOptionsProvider.cs
@@ -1,10 +1,14 @@
+using Gerulus.Core.Config;
+
 namespace Gerulus.Core.Database;
 
 public class DefaultDatabaseOptionsProvider : IDatabaseOptionsProvider
 {
+    public required IConfigProvider Config { get; init; }
+
     public Task<DatabaseOptions> ReadAsync() => Task.FromResult(new DatabaseOptions()
     {
-        ConnectionString = "Data Source=Gerulus.db",
+        ConnectionString = $"Data Source={Config.Get().DatabaseFileLocation}",
         IsInMemory = false
     });
 }
