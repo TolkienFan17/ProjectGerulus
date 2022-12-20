710d89a0c0337c1e0765b97ccc00342f817d27f9
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2022-12-20 15:17:57 -0600

    Implements file saving and loading, and migrates to sealed class DHFG

--- Gerulus.Core/Crypto/DHFG/DHFGFileParameterProvider.cs
+++ Gerulus.Core/Crypto/DHFG/DHFGFileParameterProvider.cs
@@ -1,25 +1,49 @@
-using Gerulus.Core.Crypto;
 using Org.BouncyCastle.Crypto.Generators;
 using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Utilities.IO.Pem;
 
-namespace Gerulus.Core.Crypto.DHFG;
+namespace Gerulus.Core.Crypto;
 
-public class FileParameterProvider : ICryptoParameterProvider<DHFGParameters>
+public sealed partial class DHFG
 {
-    public bool IsInitialized { get; private set; }
-    public DHFGParameters? Parameters { get; private set; }
-
-    public Task<DHFGParameters> GenerateParametersAsync()
+    public class FileParameterProvider : ICryptoParameterProvider<Parameters>
     {
-        var generator = new DHParametersGenerator();
-        generator.Init(4096, 100, new SecureRandom());
-        Parameters = new DHFGParameters(generator.GenerateParameters());
-        return Task.FromResult((DHFGParameters)Parameters);
-    }
+        public bool IsInitialized { get; private set; }
+        public Parameters? Parameters { get; private set; }
 
-    public Task InitializeParametersAsync(DHFGParameters parameters)
-    {
-        Parameters = parameters;
-        return Task.CompletedTask;
+        public Task<Parameters> GenerateParametersAsync()
+        {
+            var generator = new DHParametersGenerator();
+            generator.Init(512, 100, new SecureRandom());
+
+            Parameters = new Parameters(generator.GenerateParameters());
+            IsInitialized = true;
+            return Task.FromResult(Parameters);
+        }
+
+        public async Task SaveParametersAsync()
+        {
+            if (Parameters is null) throw new InvalidOperationException("Cannot save parameters until they have been generated");
+
+            using var fileWriter = new StreamWriter("dh-parameters.key");
+            var pemWriter = new PemWriter(fileWriter);
+
+            pemWriter.WriteObject(new PemObject("P Value", Parameters.P));
+            pemWriter.WriteObject(new PemObject("G Value", Parameters.G));
+            await fileWriter.FlushAsync();
+        }
+
+        public Task<bool> LoadParametersAsync()
+        {
+            using var fileReader = new StreamReader("dh-parameters.key");
+            var pemReader = new PemReader(fileReader);
+
+            var pValue = pemReader.ReadPemObject();
+            var gValue = pemReader.ReadPemObject();
+
+            Parameters = new Parameters(pValue.Content, gValue.Content);
+            IsInitialized = true;
+            return Task.FromResult(true);
+        }
     }
 }
