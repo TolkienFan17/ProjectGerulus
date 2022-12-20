b840c6dd56a8ce3186a4c102e0e6ec4d95198259
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2022-12-20 15:13:16 -0600

    Migrates key provider to sealed class structure

--- Gerulus.Core/Crypto/DHFG/DHFGKeyProvider.cs
+++ Gerulus.Core/Crypto/DHFG/DHFGKeyProvider.cs
@@ -3,37 +3,40 @@ using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Math;
 using Org.BouncyCastle.Security;
 
-namespace Gerulus.Core.Crypto.DHFG;
+namespace Gerulus.Core.Crypto;
 
-public class KeyProvider : ICryptoKeyProvider
+public sealed partial class DHFG
 {
-    public required ICryptoParameterProvider<DHFGParameters> ParameterProvider { get; init; }
-
-    public Task<CryptographicKeyPair> GenerateKeyPairAsync()
+    public class KeyProvider : ICryptoKeyProvider
     {
-        DHFGParameters parameters = ParameterProvider.IsInitialized ?
-                        ParameterProvider.Parameters! :
-                        throw new InvalidOperationException();
+        public required ICryptoParameterProvider<Parameters> ParameterProvider { get; init; }
 
-        var generator = GeneratorUtilities.GetKeyPairGenerator("DH");
-        generator.Init(new DHKeyGenerationParameters(new SecureRandom(), parameters.ToBouncyCastle()));
-        var pair = generator.GenerateKeyPair();
-        var publicPart = ((DHPublicKeyParameters)pair.Public).Y.ToByteArray();
-        var privatePart = ((DHPrivateKeyParameters)pair.Private).X.ToByteArray();
-        return Task.FromResult(new CryptographicKeyPair(publicPart, privatePart));
+        public Task<CryptographicKeyPair> GenerateKeyPairAsync()
+        {
+            Parameters parameters = ParameterProvider.IsInitialized ?
+                            ParameterProvider.Parameters! :
+                            throw new InvalidOperationException();
+
+            var generator = GeneratorUtilities.GetKeyPairGenerator("DH");
+            generator.Init(new DHKeyGenerationParameters(new SecureRandom(), parameters.ToBouncyCastle()));
+            var pair = generator.GenerateKeyPair();
+            var publicPart = ((DHPublicKeyParameters)pair.Public).Y.ToByteArray();
+            var privatePart = ((DHPrivateKeyParameters)pair.Private).X.ToByteArray();
+            return Task.FromResult(new CryptographicKeyPair(publicPart, privatePart));
+        }
+
+        public Task<byte[]> ComputeSharedKeyAsync(CryptographicKeyPair initiator, CryptographicKeyPair otherKey)
+        {
+            Parameters parameters = ParameterProvider.IsInitialized ?
+                            ParameterProvider.Parameters! :
+                            throw new InvalidOperationException();
+
+            var agreement = AgreementUtilities.GetBasicAgreement("DH");
+            agreement.Init(new DHPrivateKeyParameters(new BigInteger(otherKey.PrivateKey), parameters.ToBouncyCastle()));
+            var publicKey = new DHPublicKeyParameters(new BigInteger(initiator.PublicKey), parameters.ToBouncyCastle());
+            var secret = agreement.CalculateAgreement(publicKey);
+            return Task.FromResult(secret.ToByteArray());
+        }
+
     }
-
-    public Task<byte[]> ComputeSharedKeyAsync(CryptographicKeyPair initiator, CryptographicKeyPair otherKey)
-    {
-        DHFGParameters parameters = ParameterProvider.IsInitialized ?
-                        ParameterProvider.Parameters! :
-                        throw new InvalidOperationException();
-
-        var agreement = AgreementUtilities.GetBasicAgreement("DH");
-        agreement.Init(new DHPrivateKeyParameters(new BigInteger(otherKey.PrivateKey), parameters.ToBouncyCastle()));
-        var publicKey = new DHPublicKeyParameters(new BigInteger(initiator.PublicKey), parameters.ToBouncyCastle());
-        var secret = agreement.CalculateAgreement(publicKey);
-        return Task.FromResult(secret.ToByteArray());
-    }
-
 }
